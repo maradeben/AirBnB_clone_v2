b2f5e81b265c5f62c7474d6b38e8011b4e0473f2
Eben <maradebenezer@gmail.com>
2023-06-22 04:56:23 +0100

    commit

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
--- Creates a MySQL server with:
---   Database hbnb_dev_db.
---   User hbnb_dev with password hbnb_dev_pwd in localhost.
+-- Creates a MySQL server named `hbnb_dev_db`
+-- Creates a user `hbnb_dev` with `password` hbnb_dev_pwd; in localhost.
 --   Grants all privileges for hbnb_dev on hbnb_dev_db.
 --   Grants SELECT privilege for hbnb_dev on performance_schema.
 
@@ -10,10 +9,8 @@ CREATE USER
     IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES
    ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   TO 'hbnb_dev'@'localhost';
 GRANT SELECT
    ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
