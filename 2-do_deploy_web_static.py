cbde28e5ba4eb3fa599dedc2fe9d4ea9cccc9a41
Eben <maradebenezer@gmail.com>
2023-07-10 01:32:42 +0100

    added 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" distribute archive to web server """
+from os import path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+
+
+env.hosts = ["54.83.167.170", "54.221.184.104"]
+
+def do_deploy(archive_path):
+    """ distribute archive to web server """
+
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+            format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+            format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+            format(name)).failed is True:
+        return False
+    return True
