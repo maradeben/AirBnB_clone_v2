516952851a34396827561b81237dbfb26f58a56c
Eben <maradebenezer@gmail.com>
2023-07-10 01:11:40 +0100

    fixed pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,15 +4,16 @@ import os.path
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     """ compress web_static directory, gzip """
     date = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
-                                                        date.month,
-                                                        date.day,
-                                                        date.hour,
-                                                        date.minute,
-                                                        date.second)
+                                                         date.month,
+                                                         date.day,
+                                                         date.hour,
+                                                         date.minute,
+                                                         date.second)
 
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
