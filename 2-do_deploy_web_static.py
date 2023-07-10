e9c670626a91e2b9636cfbdc42edfff1e3e74dd1
Eben <maradebenezer@gmail.com>
2023-07-10 01:46:38 +0100

    fixing 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,42 @@
 #!/usr/bin/python3
-""" distribute archive to web server """
-from os import path
+# distribute archive to web server
+import os.path
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
 
-
-env.hosts = ["54.83.167.170", "54.221.184.104"]
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_deploy(archive_path):
-    """ distribute to web server """
-
-    if path.isfile(archive_path) is False:
+    """ distro to web server """
+    if os.path.isfile(archive_path) is False:
         return False
-    file = archive_path.split("/")[-]
+    file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-            format(name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-            format(name)).failed is True:
+           format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-            format(file, name)).failed is True:
+           format(file, name)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(name, name)).failed is True:
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(name)).failed is True:
+           format(name)).failed is True:
         return False
     return True
