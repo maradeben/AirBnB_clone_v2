857f14f16ae898ef700932a0918c910581b88175
Eben <maradebenezer@gmail.com>
2023-07-22 09:16:56 +0100

    adding README.md

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" script to start a flask web app """
+""" script to start a flask web app, adding the /hbnb route """
+
 
 from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello_hello():
     """ print 'Hello HBNB' """
     return ("Hello HBNB")
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ print 'HBNB' """
     return ("HBNB")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
